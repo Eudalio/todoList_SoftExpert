76e99171f26d6c35893cadbc96c6a28af0b7f956
03219759360 <tonnydallios@gmail.com>
2018-12-12 10:05:00 -0300

    [#1] WEB-101

--- src/js/events.js
+++ src/js/events.js
@@ -9,6 +9,15 @@ export function registerEventHandlers() {
         event.stopPropagation();
     });
 
+    listen('keypress', '#todoInput', event => {
+        if (event.key === 'Enter' && event.which === 13) {
+            event.preventDefault();
+            const todoInput = document.getElementById('todoInput');
+            todos.dispatch(addTodo(todoInput.value));
+            event.stopPropagation();
+        }
+    })
+
     listen('click', '.js_toggle_todo', event => {
         const id = Number.parseInt(event.target.getAttribute('data-id'), 10);
         todos.dispatch(toggleTodoState(id));
